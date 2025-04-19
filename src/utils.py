c2e3891d8d577beeaff03fa3b076d1e4db96cfe8
Yuntian Deng <dengyuntian@gmail.com>
2025-04-19 00:26:26 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -36,11 +36,11 @@ def get_sep_position(input_ids, sep_id, skip=0):
 class DoubleEOSStoppingCriteria(StoppingCriteria):
     def __init__(self, eos_token_id):
         super().__init__()
-        self.eos_token_id = eos_token_id
+        self.eos_token_id_ = eos_token_id
         self.init = False
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
-        eos_count = (input_ids == self.eos_token_id).sum(dim=-1)
+        eos_count = (input_ids == self.eos_token_id_).sum(dim=-1)
         if not self.init:
             self.init = True
             self.eos_count_init = eos_count
@@ -50,16 +50,16 @@ class DoubleEOSStoppingCriteria(StoppingCriteria):
 class DoubleEOSLogitsProcessor(LogitsProcessor):
     def __init__(self, eos_token_id):
         super().__init__()
-        self.eos_token_id = eos_token_id
+        self.eos_token_id_ = eos_token_id
         self.init = False
     
     def __call__(self, input_ids, scores):
-        eos_count = (input_ids == self.eos_token_id).sum(dim=-1)
+        eos_count = (input_ids == self.eos_token_id_).sum(dim=-1)
         if not self.init:
             self.init = True
             self.eos_count_init = eos_count
         done = (eos_count - self.eos_count_init) >= 2
         if done.any():
             scores[done, :] = float('-inf')
-            scores[done, self.eos_token_id] = 0
+            scores[done, self.eos_token_id_] = 0
         return scores
